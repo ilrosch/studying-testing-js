710aef74914c24b97aa1b4be5b1094a9f9afd4ea
Ilya Roschikhmarov <ilrosch@mail.ru>
2024-01-11 13:27:37 +0300

    update test (assert)

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,15 @@
+import { strict as assert } from 'node:assert';
 import { capitalize } from "../src/capitalize.js";
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Function does not work correctly!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//     throw new Error('Function does not work correctly!');
+// }
 
-if (capitalize('') !== '') {
-    throw new Error('Function does not work correctly!');
-}
+// if (capitalize('') !== '') {
+//     throw new Error('Function does not work correctly!');
+// }
+
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
 
 console.log('Tests is passed!')
